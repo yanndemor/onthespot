be054d6cff8cc1c93cc1b3dcda208a5f8a51a8f0
yanndemor <yann.demor@gmail.com>
2021-03-23 17:12:11 +0100

    mise à jour aprés galére sur git suite au conflit lors du merge de cgu

---
+++ src/components/CGV/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable max-len */
+import React from 'react';
+import './cgv.scss';
+
+const CGV = () => (
+  <div className="cgv-content">
+    <div className="cgv-title">
+      <h2>Conditions Générale de ventes</h2>
+    </div>
+   
+    <div className="cgv-text">
+      <p>Les conditions générales de vente constituent la base de votre négociation commerciale et de vos relations commerciales. Lorsque vous vous lancez dans une activité de vente de produits ou de prestations de service en B to B (Business to Business) ou B to C (Business to Customer), vous devez informer vos clients de vos conditions générales de vente. Ces conditions écrites permettent de sécuriser vos relations commerciales. Les clauses peuvent être différentes selon que vos produits ou services concernent des consommateurs particuliers ou des professionnels. </p>
+    </div>
+    
+  </div>
+
+);
+
+export default CGV;
