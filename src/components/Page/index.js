cc62939d92005f6ac29a5ea0af8b3135613044c2
Joe971 <jho_apa@hotmail.com>
2021-03-23 13:58:24 +0100

    bug fixes

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {
   Switch,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 import Products from 'src/components/Products';
 
 const Page = () => (
-<div className="page">
+  <div className="page">
     <div className="page-content">
       <Switch>
         <Route path="/products/">
