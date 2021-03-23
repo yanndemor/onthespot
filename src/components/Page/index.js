696b08401f9bc55404899c0b74b740a9b3926fa9
yanndemor <yann.demor@gmail.com>
2021-03-23 16:31:34 +0100

    component CGV

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {
   Switch,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 import Products from 'src/components/Products';
+import CGV from 'src/components/CGV';
 
 const Page = () => (
-<div className="page">
+  <div className="page">
     <div className="page-content">
       <Switch>
         <Route path="/products/">
           <Products />
+        </Route>
+        <Route path="/cgv">
+          <CGV />
         </Route>
         <Route path="/">
           <div>HomePage</div>
