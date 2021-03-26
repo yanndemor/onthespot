f2f1a4a67c7c2245693d56ad43a5a04f9d55f862
Joe971 <jho_apa@hotmail.com>
2021-03-26 10:57:31 +0100

    css oss

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -15,15 +15,17 @@ const Order = () => (
   // </article>
 
   <div className="order-detail row">
-    <div className="order-name text-center ">
-      <p>Commande XXX</p>
-    </div>
-    <div className="order-description col-6">
-      <p className="w-75 text-center mx-auto">Détail de la commande</p>
-      <div className="mx-2">
-        <li>XXX Beignet</li>
-        <li>XXX Beignet</li>
-        <li>XXX Beignet</li>
+    <div className="col-8">
+      <div className="order-name text-center ">
+        <p>Commande XXX</p>
+      </div>
+      <div className="order-description ">
+        <p className="w-75 text-center">Détail de la commande</p>
+        <div className="mx-2">
+          <li>XXX Beignet</li>
+          <li>XXX Beignet</li>
+          <li>XXX Beignet</li>
+        </div>
       </div>
     </div>
     <div className="order-state col-4">
