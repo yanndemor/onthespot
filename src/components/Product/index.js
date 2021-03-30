7734ab47e8b2243bcb0fc8bf16db48da11144f53
yanndemor <yann.demor@gmail.com>
2021-03-30 22:18:57 +0200

    css page accueill

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -38,14 +38,14 @@ const Product = ({ productsList, categoriesList }) => {
               </div>
 
             </div>
-            <div className="col my-2 py-2 product-description">
+            <div className="col my-2 py-4 product-description">
               <p className=" text-center  my-2">Détail du produit</p>
               <div className="mx-2">
                 <p>{theProduct.description}</p>
               </div>
             </div>
 
-            <div className="d-flex  my-2 align-items-center justify-content-center text-center">
+            <div className="d-flex  my-4 align-items-center justify-content-center text-center">
               <div className="button-minus ">
                 <ButtonDown />
               </div>
