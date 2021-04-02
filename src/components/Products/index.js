da59103659718dd89d677fa8f63e4f1f42b34c9a
Josso Cedric <cjosso@gmail.com>
2021-04-02 20:00:42 +0200

    Widget cart from page Products is complete

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 /* import Product from 'src/components/Product'; */
-import { home } from 'react-feather';
-import ButtonUp from 'src/components/ButtonUp';
-import ButtonDown from 'src/components/ButtonDown';
 import LoadingSpinner from 'src/components/LoadingSpinner';
 import ButtonAddCart from 'src/containers/ButtonAddCart';
 
@@ -12,7 +9,6 @@ import { Link, useParams } from 'react-router-dom';
 
 /* import RecipeSmall from 'src/containers/Home/RecipeSmall'; */
 import './products.scss';
-import Cart from 'src/components/Cart';
 
 const Products = ({ productsList, loading, orderProducts }) => {
   console.log('the products in products', productsList);
@@ -33,6 +29,11 @@ const Products = ({ productsList, loading, orderProducts }) => {
       </div>
     );
   }
+
+  let total = 0;
+  orderProducts.forEach((product) => {
+    total += product.price * product.quantity;
+  });
 
   return (
     <main>
@@ -61,9 +62,9 @@ const Products = ({ productsList, loading, orderProducts }) => {
         <div className="productsList-cart">
           <div className="productsList-cart-recap">
             <div className="productsList-cart-number">
-              Panier <span className="badge badge-light">4</span>
+              Panier <span className="badge badge-light">{orderProducts.length}</span>
             </div>
-            <div className="productsList-cart-total"><span>total : </span>2235.45€</div>
+            <div className="productsList-cart-total"><span>total : </span>{total} €</div>
           </div>
           <div className="productsList-cart-details">
             {cart}
@@ -80,18 +81,8 @@ const Products = ({ productsList, loading, orderProducts }) => {
               </div>
             ))}
 
-            {/* <div className="productsList-cart-item">
-              <div className="item-picture">
-                <img src="https://www.onthespot.link/back/public/assets/images/magnum-vanille_th.png" alt="Magnum cocholat" />
-              </div>
-              <div className="item-details">
-                <div className="item-name">Magnum racing</div>
-                <div className="item-price float-left">526.21 € <span className="item-quantity text-muted mr-0 float-right">Qtt: 05</span></div>
-              </div>
-            </div> */}
-
           </div>
-          <a href="#" className="btn btn-primary w-100">Voir le panier</a>
+          <Link to="/panier" className="btn btn-primary w-100">Voir le panier</Link>
         </div>
       </>
       )}
