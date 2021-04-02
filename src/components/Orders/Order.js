84ebd33defce0f58b1b1a1e22f7480bc3d18c5d9
Jhoanny <jho_apa@hotmail.com>
2021-04-02 14:14:31 +0200

    implementation of products in orders with total amount

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -5,49 +5,56 @@ const Order = ({
   id, status, deliveryTime, orderProducts,
 }) => {
   console.log(deliveryTime);
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const totalCart = orderProducts.map((productItem) => productItem.product.price * productItem.quantity).reduce(reducer);
+
   return (
     <div className="order-detail row">
       <div className="order-name text-center ">
         <p>Commande {id}</p>
       </div>
-      {orderProducts.map((productItem) => (
-        <>
-          <div key={productItem.id} className="col-8">
-            <div className="order-description ">
-              <p className="w-75 text-center">Détail de la commande</p>
-              <div className="mx-2">
-                <li>{productItem.product.name},
+      <div className="col-8">
+        <div className="order-description ">
+          <p className="w-75 text-center">Détail de la commande</p>
+          <div className="mx-2">
+            {orderProducts.map((productItem) => (
+              <>
+                <li key={productItem.id}>
+                  {productItem.product.name}
                   quantité : {productItem.quantity}
                   prix : {productItem.product.price}
                 </li>
-              </div>
-            </div>
+              </>
+            ))}
           </div>
-          <div className="order-state col-4">
-            <div className="order-date ">
-              <p>Le {deliveryTime}</p>
-            </div>
-            <div className="order-status ">
-              <p>
-                {
-            status === 0 && <div>Status : En attente </div>
-            || status === 1 && <div>Status : En préparation</div>
-            || status === 2 && <div>Status : En livraison </div>
-            || status === 3 && <div>Status : Livrée </div>
+        </div>
+        <div className="order-date ">
+          <p>Le {deliveryTime}</p>
+        </div>
+        <div className="order-status ">
+          <div>
+            {
+              status === 0 && <div>Status : En attente </div>
+              || status === 1 && <div>Status : En préparation</div>
+              || status === 2 && <div>Status : En livraison </div>
+              || status === 3 && <div>Status : Livrée </div>
             }
-              </p>
-            </div>
-
-            <div className="order-price">
-              <p> Montant = {`${productItem.product.price * productItem.quantity} €`}</p>
-            </div>
           </div>
-        </>
-      ))}
+        </div>
+      </div>
+
+      <div className="order-state col-4">
+        <div className="order-price">
+          <p>
+            Montant Total :{`${totalCart} €`}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
-
+/* {`${productItem.product.price * productItem.quantity} €`} */
 Order.propTypes = {
   id: PropTypes.number.isRequired,
   status: PropTypes.number.isRequired,
@@ -63,3 +70,7 @@ Order.propTypes = {
 }.isRequired;
 
 export default Order;
+
+// const testSum = (accumulator, currentValue) => accumulator + currentValue;
+//   const totalCart = productItems.map((product) => product.price * product.quantity);
+//   alert(totalCart.reduce(testSum));
