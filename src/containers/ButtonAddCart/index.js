c01c16c71f8f2655ed3e8a42238274e6f0a85f80
Josso Cedric <cjosso@gmail.com>
2021-04-04 07:24:00 +0200

    button add to cart/remove/update quantity is fully working

--- src/containers/ButtonAddCart/index.js
+++ src/containers/ButtonAddCart/index.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 
-import { addCart } from 'src/actions/cart';
+import { addCart, quantityLess, updateQuantity } from 'src/actions/cart';
 
 // on importe le composant de présentation
 import ButtonAddCart from 'src/components/ButtonAddCart';
@@ -9,14 +9,21 @@ import ButtonAddCart from 'src/components/ButtonAddCart';
 // si on a besoin de lire des informations dans le state
 const mapStateToProps = (state) => ({
   // nom de la prop à remplir: élément à récupérer dans le state
+  orderProducts: state.cart.orderProducts,
 });
 
 // === mapDispatchToProps
 // si on a besoin de dispatcher des actions vers le store (mettre à jour le state)
 const mapDispatchToProps = (dispatch) => ({
   // nom de la prop à remplir: fonction qui dispatch l'action
-  handleAddCart: (name) => {
-    dispatch(addCart(name));
+  manageAddCart: (product) => {
+    dispatch(addCart(product));
+  },
+  manageRemoveCart: (product) => {
+    dispatch(quantityLess(product));
+  },
+  manageQtyChange: (product, qty) => {
+    dispatch(updateQuantity(product, qty));
   },
 });
 
