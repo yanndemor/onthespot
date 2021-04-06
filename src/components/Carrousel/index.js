90237be59113f5145f1ae1da555ad556093bdb67
yanndemor <yann.demor@gmail.com>
2021-04-06 22:13:10 +0200

    back to the past and usage of cocost code with index of an array created with the productslist

--- src/components/Carrousel/index.js
+++ src/components/Carrousel/index.js
@@ -15,93 +15,37 @@ const Carrousel = ({ productsList }) => {
   console.log('carrousel');
   return (
     <div className="carrousel">
-      <div className="container text-center my-3">
-        <h2 className="font-weight-light">Bootstrap 4 - Multi Item Carousel</h2>
-        <div className="row mx-auto my-auto">
-          <div id="recipeCarousel" className="carousel slide w-100" data-ride="carousel">
-            <div className="carousel-inner w-100" role="listbox">
-              <div className="carousel-item active">
-                <div className="col-md-4">
-                  <div className="card card-body">
-                    <img
-                      className="img-fluid"
-                      src={`https://www.onthespot.link/back/public/${images[5]}`}
-                      alt="First slide"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="col-md-4">
-                  <div className="card card-body">
-                    <img
-                      className="img-fluid"
-                      src={`https://www.onthespot.link/back/public/${images[8]}`}
-                      alt="First slide"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="col-md-4">
-                  <div className="card card-body">
-                    <img
-                      className="img-fluid"
-                      src={`https://www.onthespot.link/back/public/${images[10]}`}
-                      alt="First slide"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="col-md-4">
-                  <div className="card card-body">
-                    <img
-                      className="img-fluid"
-                      src={`https://www.onthespot.link/back/public/${images[14]}`}
-                      alt="First slide"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="col-md-4">
-                  <div className="card card-body">
-                    <img
-                      className="img-fluid"
-                      src={`https://www.onthespot.link/back/public/${images[18]}`}
-                      alt="First slide"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="col-md-4">
-                  <div className="card card-body">
-                    <img
-                      className="img-fluid"
-                      src={`https://www.onthespot.link/back/public/${images[20]}`}
-                      alt="First slide"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <a className="carousel-control-prev w-auto" href="#recipeCarousel" role="button" data-slide="prev">
-              <span className="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true" />
-              <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next w-auto" href="#recipeCarousel" role="button" data-slide="next">
-              <span className="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true" />
-              <span className="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-
-      </div>
+      <Carousel>
+        <Carousel.Item >
+          <img
+            className="image"
+            s
+            src={`https://www.onthespot.link/back/public/${images[6]}`}
+            alt="First slide"
+          />
+         
+        </Carousel.Item>
+        <Carousel.Item >
+          <img
+            className="image"
+            src={`https://www.onthespot.link/back/public/${images[15]}`}
+            alt="Second slide"
+          />
+        
+        </Carousel.Item>
+        <Carousel.Item >
+          <img
+            className="image"
+            src={`https://www.onthespot.link/back/public/${images[10]}`}
+            alt="Third slide"
+          />
+         
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
 
 // == Export
 export default Carrousel;
+
