bb582e0368314c77ee56f42034375b5ff8137cd2
yanndemor <yann.demor@gmail.com>
2021-04-06 22:02:29 +0200

    essai carroussel

--- src/components/Carrousel/index.js
+++ src/components/Carrousel/index.js
@@ -3,48 +3,102 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 // == Import
 import './carrousel.scss';
-
+/* import './carousel.js'; */
 // == Composant
 const Carrousel = ({ productsList }) => {
+  const products = productsList.filter((product) => product.picture);
+
+  const images = [];
+  productsList.forEach((product) => {
+    images.push(product.picture);
+  });
   console.log('carrousel');
   return (
     <div className="carrousel">
-      <Carousel>
-        {productsList.map((product) => (
-          <Carousel.Item interval={1500}>
-            <img
-              className="image"
-              src={`https://www.onthespot.link/back/public/${product.picture}`}
-              alt="First slide"
-            />
-           
-          </Carousel.Item>
+      <div className="container text-center my-3">
+        <h2 className="font-weight-light">Bootstrap 4 - Multi Item Carousel</h2>
+        <div className="row mx-auto my-auto">
+          <div id="recipeCarousel" className="carousel slide w-100" data-ride="carousel">
+            <div className="carousel-inner w-100" role="listbox">
+              <div className="carousel-item active">
+                <div className="col-md-4">
+                  <div className="card card-body">
+                    <img
+                      className="img-fluid"
+                      src={`https://www.onthespot.link/back/public/${images[5]}`}
+                      alt="First slide"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className="col-md-4">
+                  <div className="card card-body">
+                    <img
+                      className="img-fluid"
+                      src={`https://www.onthespot.link/back/public/${images[8]}`}
+                      alt="First slide"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className="col-md-4">
+                  <div className="card card-body">
+                    <img
+                      className="img-fluid"
+                      src={`https://www.onthespot.link/back/public/${images[10]}`}
+                      alt="First slide"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className="col-md-4">
+                  <div className="card card-body">
+                    <img
+                      className="img-fluid"
+                      src={`https://www.onthespot.link/back/public/${images[14]}`}
+                      alt="First slide"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className="col-md-4">
+                  <div className="card card-body">
+                    <img
+                      className="img-fluid"
+                      src={`https://www.onthespot.link/back/public/${images[18]}`}
+                      alt="First slide"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className="col-md-4">
+                  <div className="card card-body">
+                    <img
+                      className="img-fluid"
+                      src={`https://www.onthespot.link/back/public/${images[20]}`}
+                      alt="First slide"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <a className="carousel-control-prev w-auto" href="#recipeCarousel" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true" />
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next w-auto" href="#recipeCarousel" role="button" data-slide="next">
+              <span className="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true" />
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
 
-        ))}
-
-        {/*      <Carousel.Item interval={500}>
-          <img
-            className=" w-100"
-            src="https://img-19.ccm2.net/8vUCl8TXZfwTt7zAOkBkuDRHiT8=/1240x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="w-100"
-            src="https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item> */}
-      </Carousel>
+      </div>
     </div>
   );
 };
