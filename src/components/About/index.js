5c152603b558f2501af8977b6491b129e5d1505d
github_pierreFerre <github_pedr1ferre@gmail.com>
2021-04-07 16:40:37 +0200

    CREATE: about us page

---
+++ src/components/About/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './about.scss';
+
+const About = () => (
+  <main>
+    <div className="about">
+  
+      <div className="container">
+        <div className="about-intro">
+          <h1>La génèse</h1>
+          <p>L'été dernier, Yann, Product Owner, sillonait les plages de l'Atlantique, distribuant sourires et rafraichissements aux vacanciers profitant du généreux soleil sur leur serviette de plage.</p>
+          <p>Pour accomplir cette mission et transporter glaces et boissons fraiches, il était contraint de tirer sur le sable une lourde charrette réfrigérée.</p>
+          <p>C'est pendant la formation qu'a germé dans son esprit l'idée d'une application qui faciliterait son travail. L'utilisateur, depuis sa serviette, n'aurait qu'à faire sa commande sur son téléphone, pour qu'un livreur vienne ensuite la livrer à un point de rendez-vous proche de l'utilisateur.</p>
+          <p>Pour mener à bien ce projet, l'équipe suivante à été mise en place.</p>
+        </div>
+  
+        <div className="about-team">
+          <h1>Les créateurs</h1>
+          <div>
+            <img></img>
+            <h2>Yann Demor</h2>
+            <p>Product Owner</p>
+            <p>Il chevauche des vagues depuis ses 12 ans et dompte désormais le web depuis O'clock. retrouvez-le ici : https://www.linkedin.com/in/yann-demor/</p>
+          </div>
+  
+          <div>
+            <img></img>
+            <h2>Cédric Costard</h2>
+            <p>Lead dev frontend</p>
+            <p></p>	  
+          </div>
+  
+          <div>
+            <img></img>
+            <h2>Jhoanny Apatout</h2>
+            <p>Git master</p>
+            <p></p>
+          </div>
+  
+          <div>
+            <img></img>
+            <h2>Cédric Josso</h2>
+            <p>Lead dev backend</p>
+            <p>Ge</p>
+          </div>
+  
+          <div>
+            <img></img>
+            <h2>Pierre Ferré</h2>
+            <p>Scrum master</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </main>
+);
+
+export default About;
