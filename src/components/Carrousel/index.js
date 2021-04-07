f0950c00bfb6c060ad6e8172a8bb7a224f135c52
yanndemor <yann.demor@gmail.com>
2021-04-07 08:28:08 +0200

    3 carroussel in a row with differents product / by categories

--- src/components/Carrousel/index.js
+++ src/components/Carrousel/index.js
@@ -15,37 +15,179 @@ const Carrousel = ({ productsList }) => {
   console.log('carrousel');
   return (
     <div className="carrousel">
-      <Carousel>
-        <Carousel.Item >
-          <img
-            className="image"
-            s
-            src={`https://www.onthespot.link/back/public/${images[6]}`}
-            alt="First slide"
-          />
-         
-        </Carousel.Item>
-        <Carousel.Item >
-          <img
-            className="image"
-            src={`https://www.onthespot.link/back/public/${images[15]}`}
-            alt="Second slide"
-          />
-        
-        </Carousel.Item>
-        <Carousel.Item >
-          <img
-            className="image"
-            src={`https://www.onthespot.link/back/public/${images[10]}`}
-            alt="Third slide"
-          />
-         
-        </Carousel.Item>
-      </Carousel>
+      <div className="container">
+
+        <div className="carrousel-line row">
+          <div className="item col-4 d-xs-none">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  s
+                  src={`https://www.onthespot.link/back/public/${images[1]}`}
+                  alt="First slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[2]}`}
+                  alt="Second slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[3]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[4]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[5]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[6]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[7]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[8]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          <div className="col-4 d-xs-none">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  s
+                  src={`https://www.onthespot.link/back/public/${images[9]}`}
+                  alt="First slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[10]}`}
+                  alt="Second slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[11]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[12]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          <div className="col-4 d-xs-none">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="image"
+                
+                  src={`https://www.onthespot.link/back/public/${images[13]}`}
+                  alt="First slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[14]}`}
+                  alt="Second slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[15]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+
+              <Carousel.Item>
+                <img
+                  className="image"
+                  s
+                  src={`https://www.onthespot.link/back/public/${images[16]}`}
+                  alt="First slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[17]}`}
+                  alt="Second slide"
+                />
+
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="image"
+                  src={`https://www.onthespot.link/back/public/${images[18]}`}
+                  alt="Third slide"
+                />
+
+              </Carousel.Item>
+            </Carousel>
+
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 };
 
 // == Export
 export default Carrousel;
-
